75faacc88ab9ef21eec241944c6bb7101b912164
jared josafhat hernandez zuñiga <jaredjhz2597@icloud.com>
2020-04-06 17:19:22 -0500

    added images

--- src/components/biography/biography.js
+++ src/components/biography/biography.js
@@ -3,21 +3,14 @@ import classes from './biography.module.css';
 import {Carousel} from 'react-responsive-carousel';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import ButtonLink from '../../components/buttonLink/buttonLink';
+import Jared from '../../static/jared.jpg';
 export default (props) => {
     return (
         <div className={classes.container}>
             <div className={classes.left}>
-            <Carousel width="300px" showIndicators={false} showStatus={false} showThumbs={false} showArrows={true}>
                 <div className={classes.contenido}>
-                    <img alt="jared" src="https://portfolio-jaredhz.s3.amazonaws.com/jared.jpg" />
+                    <img alt="jared" src={Jared} />
                 </div>
-                <div className={classes.contenido}>
-                    <img alt="jared2" src="https://portfolio-jaredhz.s3.amazonaws.com/IMG_20180420_092145.jpg" />
-                </div>
-                <div className={classes.contenido}>
-                    <img alt="jared3" src="https://portfolio-jaredhz.s3.us-east-1.amazonaws.com/IMG_20180530_101848.jpg" />
-                </div>
-            </Carousel>
             </div>
             <div className={classes.right}>
                 <div className={classes.contenidoBiografia}>
